2ab8cc90a5138a4d760a8c403647c2e2fc824909
Ace-Nanter <ace.nanter@hotmail.fr>
2017-02-15 23:21:37 +0100

    View routing OK

--- app.js
+++ app.js
@@ -1,23 +1,50 @@
-var app = angular.module('StackOverflowClient', ['pascalprecht.translate']);
+var app = angular.module('StackOverflowClient',
+    [
+        'pascalprecht.translate',
+        'ngRoute'
+    ]);
 
 app.config(
-    ['$translateProvider', function ($translateProvider) {
+    [
+        '$httpProvider',
+        '$routeProvider',
+        '$locationProvider',
+        '$translateProvider',
+        function($httpProvider, $routeProvider, $locationProvider, $translateProvider) {
+
+        // httpProvider
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+
+        // locationProvider
+        $locationProvider.hashPrefix("!");
+        //$locationProvider.html5Mode(true);
+
+        // routeProvider
+        $routeProvider
+            .when("/", {
+                templateUrl: 'views/question/index.html',
+                controller: 'QuestionController'
+            })
+            .when("/questions", {
+                templateUrl: 'views/question/index.html',
+                controller: 'QuestionController'
+            })
+            .otherwise({
+                templateUrl : "views/errors/error404.html"
+            });
+
+        // translateProvider
         $translateProvider
             .translations('en', translations_en)
             .translations('fr', translations_fr)
             .preferredLanguage('fr');
-    }],
-    ['httpProvider', function($httpProvider) {
-        $httpProvider.defaults.useXDomain = true;
-        delete $httpProvider.defaults.headers.common['X-Requested-With'];
-    }],
-    [ function ($locationProvider) {
-        $locationProvider.html5Mode(true);
     }]
 );
 
+/*
 app.controller('Ctrl', function ($scope, $translate) {
     $scope.changeLanguage = function (key) {
         $translate.use(key);
     };
-});
+});*/
--- controllers/QuestionController.js
+++ controllers/QuestionController.js
@@ -1,9 +1,9 @@
 app.controller("QuestionController", function($scope, $http){
     $scope.yolo = "Hello World !";
     $scope.inputValue= "";
-
+/*
     request($http, "http://localhost:8080", 'GET', function(response) {
         $scope.questions = response.data;
-    });
+    });*/
 });
 

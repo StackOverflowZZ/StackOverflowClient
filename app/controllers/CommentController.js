6c79ab07902c79034b12b8687488fd11b267c877
Haecriver <pierre_chevalier@orange.fr>
2017-03-18 18:19:01 +0100

    Creation comment

--- app/controllers/CommentController.js
+++ app/controllers/CommentController.js
@@ -1,10 +1,18 @@
 app.controller("CommentController",
     function($scope, $route, $location, Comment) {
 
-    $scope.createComment = function(comment){
+    $scope.createComment = function(comment, idAnswer, idQuestion){
+
+        if(idAnswer!=null){
+            comment.answer={'id':idAnswer}
+        }
+
+        if(idQuestion!=null){
+            comment.question={'id':idQuestion}
+        }
 
         // Post to the endpoint
-        Comment.create(comment, function (idQuestion) {
+        Comment.create(comment, function () {
             $route.reload()
         });
     };
